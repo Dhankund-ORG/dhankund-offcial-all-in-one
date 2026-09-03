4dc15262bbf409f9266bf77235a18f49241b18e0
Dheerendra Tripathi <153520649+acharypdt@users.noreply.github.com>
2026-09-03 16:01:41 +0530

    fix: correct mojibake characters

--- DSA-Banker-flutter app/lib/presentation/customer_app/loan_selection_screen.dart
+++ DSA-Banker-flutter app/lib/presentation/customer_app/loan_selection_screen.dart
@@ -1,4 +1,4 @@
-﻿import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:my_flutter_app/presentation/customer_app/loan_application_screen.dart';
 
 class LoanSelectionScreen extends StatelessWidget {
